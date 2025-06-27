59e7c99811e3d8431fb3352a9d8d4757edeb9b39
srullsim <simchonisrael@gmail.com>
2025-06-27 11:50:58 +0300

    add CRUD

--- manager.py
+++ manager.py
@@ -2,8 +2,8 @@ from DAL import  Dal
 import  agent
 from agent import Agent
 
-a = Agent("avi", "NY")
+a = Agent("ben", "NY")
 d = Dal()
 
-d.add_agent_to_db(a)
-d.get_all_agents()
+# d.add_agent_to_db(a)
+d.update_agent_missionsCompleted(1, "avi")
